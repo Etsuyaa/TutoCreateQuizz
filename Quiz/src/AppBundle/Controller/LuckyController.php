8fbc83f3da3775eb8a7dd280aa5f2f82332d82c7
TutoCreateQuiz <36199834+LamicaleWAC@users.noreply.github.com>
2018-05-03 15:55:22 +0200

    up
    up

---
+++ Quiz/src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    /**
+     * @Route("/lucky")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
